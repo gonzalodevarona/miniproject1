bf8a945874e8e491f015def588fe6d23aec492fd
gonzalodevarona <gonzalo.de1@correo.icesi.edu.co>
2021-03-16 18:22:50 -0500

    finished

--- independiente.m
+++ independiente.m
@@ -12,8 +12,8 @@ t1 = -20:0.01:20;
 
 %SEÑAL CUADRADA
 
-A0 = 0.1; %Amplitud cuadrada
-F0 = 0.1; %Frecuencia cuadrada
+A0 = 1; %Amplitud cuadrada
+F0 = 1; %Frecuencia cuadrada
 Fase0 = 0; %Fase cuadrada
 
 x = A0*square(2*pi*F0*t1+Fase0);
@@ -26,12 +26,12 @@ xlabel('t');
 
 
 %SEÑAL SENOIDAL
-A1 = 0.1; %Amplitud seno
-F1 = 0.1; %Frecuencia seno
+A1 = 1; %Amplitud seno
+F1 = 1; %Frecuencia seno
 Fase1 = 0; %Fase seno
 
 
-y = A1*sin(2*pi*F1*(t1)+Fase1);
+y = A1*sin(2*pi*F1*t1+Fase1);
 
 subplot(2,2,2);
 plot(t1,y);
@@ -42,7 +42,7 @@ xlabel('t');
 
 tSum = t1+x;
 
-z = A1*sin(2*pi*F1*(t1+x)+Fase1);
+z = A1*sin((2*pi*F1*tSum) +Fase1);
 
 subplot(2,2,[3,4]);
 plot(tSum,z);
